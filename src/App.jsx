cc7c7200dccdf49a7f5ce0a35762028f40397fab
KumarShashank <s.ambastha2002@gmail.com>
2024-09-11 23:21:47 +0530

    Description of the changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Background from './components/Background';
+import Foreground from './components/Foreground';
+
+function App() {
+  return (
+    <div className='relative bg-zinc-800 h-screen w-full'>
+      <Background/>
+      <Foreground/>
+    </div>
+  )
+}
+
+export default App
